0a973ea647a88b1f6befc27536c1f37735ad093c
Maxim Polyakov <polyakovmaxim05@gmail.com>
2022-10-22 17:50:54 -0400

    commit tree c5397dc2d181cc23a0c7d8ccc631d7449e93ee5c

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+function reverse(word){
+  return word.split('').reverse().join('') 
 }
 
+function isPalindrome(word) {
+  let reversedWord = reverse(word);
+  if(reversedWord === word) return true;
+  else return false;
+}
+
+
+
 /* 
-  Add your pseudocode here
+  create variable that will represent reversed version of word
+  compare those two words
+  if they are return true
+  if they don't return false
 */
 
 /*
   Add written explanation of your solution here
-*/
+  I will create a variable that represents a reversed version of a word, named reversedWord. Then I will compare reversedWord and word, and if they are equal I will return true, othervise return false*/
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
+  console.log('Expecting: false')
+  console.log('=>', isPalindrome('maxim'));
+
+  console.log("");
+
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
